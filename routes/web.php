c31d9630c8c10b76876ce0c517cb8885a854b1ff
bocchi <boccfiap@outlook.com>
2026-05-06 22:22:47 -0300

    fazendo route que pega o dado do controller, e usando para rota

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\SeriesController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// rota criada para informada para onde vai ser enviado,
+//Route - rota | get - pegar | '/series' - pagina de acesso | SeriesController - classe | index - nome da função
+Route::get('/series', [SeriesController::class, 'index']);
