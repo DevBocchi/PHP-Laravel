adc889f92385fd5d5170b5cd8df68c6072117391
bocchi <boccfiap@outlook.com>
2026-05-12 22:03:08 -0300

    adicionando a tela de adicao de Serie

---
+++ resources/views/series/create.blade.php
@@ -0,0 +1,7 @@
+{{-- Adiciona nova serie --}}
+<x-layout title="Nova Serie">
+    <from action="" method="post">
+        <label for="nome">Nome:</label>
+        <input type="text" id="nome" name="nome">
+    </from>
+</x-layout>
